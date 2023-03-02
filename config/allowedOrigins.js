5753b0213eaca551dcf46498dd44c50295c79c1b
TamlynSpringer <tamlynspringer@gmail.com>
2023-03-02 17:21:14 +0100

    Updated cors allowed origins for FE and BE deployments

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,11 +1,13 @@
 const allowedOrigins = [
-  'https://www.yoursite.com',
+  'https://wild-erin-buffalo-kit.cyclic.app/',
+  'https://baltic-store-api.cyclic.app/',
   'http://127.0.0.1:8000',
   'http://localhost:8000',
   'http://127.0.0.1:5173',
   'http://localhost:5173',
   'http://localhost:5173/register',
   'http://localhost:5173/auth',
+  'https://baltic-store.netlify.app/'
 ];
 
 export default allowedOrigins;
